4f272ecf6a22140effe0018c5aa17f9be0219080
Iskander <obivan74@live.ru>
2020-05-09 05:37:24 +0500

    animation, images

---
+++ smooth transition.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('a[href^="#"]').on('click', function(event) {
+      // отменяем стандартное действие
+      event.preventDefault();
+      
+      var sc = $(this).attr("href"),
+          dn = $(sc).offset().top;
+      
+      $('html, body').animate({scrollTop: dn}, 1000);
+
+    });
+  });
